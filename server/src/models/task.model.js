c3eb54e113aa07c1cadf77eb443782a68d70cf21
Milos Srejic <srejic.milos94@gmail.com>
2025-03-06 23:08:51 +0100

    Create task model

---
+++ server/src/models/task.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 99,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["high", "medium", "low"],
+      default: "low",
+    },
+  },
+  { timestamps: true }
+);
+
+taskSchema.index({ status: 1 });
+taskSchema.index({ priority: 1 });
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
