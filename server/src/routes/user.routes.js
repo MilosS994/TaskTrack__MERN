c39f27a2519bfe3572900afc110a839f8241c978
Milos Srejic <srejic.milos94@gmail.com>
2025-03-06 23:15:42 +0100

    Create test user routes

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+const userRouter = Router();
+
+userRouter.get("/", async (req, res) => {
+  res.send("GET all users");
+});
+
+userRouter.get("/:id", async (req, res) => {
+  res.send("GET single user");
+});
+
+userRouter.post("/", async (req, res) => {
+  res.send("POST new user");
+});
+
+userRouter.put("/:id", async (req, res) => {
+  res.send("UPDATE user");
+});
+
+userRouter.delete("/:id", async (req, res) => {
+  res.send("DELETE user");
+});
