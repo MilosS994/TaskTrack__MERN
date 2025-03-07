4211ee79452ea3639d119414614ec383ec26f2dd
Milos Srejic <srejic.milos94@gmail.com>
2025-03-07 22:34:23 +0100

    Add error handling middleware

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ import cookieParser from "cookie-parser";
 import connectDB from "./src/database/mongodb.js";
 import { PORT } from "./src/config/env.js";
 
+import { userRouter } from "./src/routes/user.routes.js";
+
+import errorMiddleware from "./src/middleware/error.middleware.js";
+
 const app = express();
 
 app.use(express.json());
@@ -13,7 +17,9 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", tasksRouter);
 
-app.get("/", (req, res) => {
+app.use(errorMiddleware);
+
+app.app.get("/", (req, res) => {
   res.send("This server is running!");
 });
 
