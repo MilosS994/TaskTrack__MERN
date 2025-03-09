61d3c1b0e1bdf3292a2479f06b9208a78bf5d943
Milos Srejic <srejic.milos94@gmail.com>
2025-03-09 18:21:47 +0100

    Fix getUserTasks code by changing userId to author and create getUserTask controller function

--- server/src/controllers/task.controller.js
+++ server/src/controllers/task.controller.js
@@ -2,7 +2,7 @@ import Task from "../models/task.model.js";
 
 export const getUserTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find({ userId: req.user._id });
+    const tasks = await Task.find({ author: req.user._id });
 
     if (!tasks || tasks.length === 0) {
       return res
@@ -11,6 +11,23 @@ export const getUserTasks = async (req, res, next) => {
     }
 
     res.status(200).json({ success: true, data: tasks });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserTask = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const task = await Task.findOne({ _id: id, author: req.user._id });
+
+    if (!task) {
+      const error = new Error("Task not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: task });
   } catch (error) {
     next(error);
   }
