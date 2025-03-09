01ce83e9517d43f29800f56d1e17b57e1286dc82
Milos Srejic <srejic.milos94@gmail.com>
2025-03-09 19:06:11 +0100

    Add controller functions to routes

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
+import { authorize } from "../middleware/auth.middleware.js";
+import {
+  getUserTasks,
+  getUserTask,
+  createUserTask,
+  updateUserTask,
+  deleteUserTask,
+} from "../controllers/task.controller.js";
 
 const tasksRouter = Router();
+
+tasksRouter.get("/", authorize, getUserTasks);
+
+tasksRouter.get("/:id", authorize, getUserTask);
+
+tasksRouter.post("/", authorize, createUserTask);
+
+tasksRouter.put("/:id", authorize, updateUserTask);
+
+tasksRouter.delete("/:id", authorize, deleteUserTask);
 
 export default tasksRouter;
