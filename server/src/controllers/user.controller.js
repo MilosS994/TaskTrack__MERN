a224ccd45c9676de87bd117d69f6203f4dac3086
Milos Srejic <srejic.milos94@gmail.com>
2025-03-09 13:03:34 +0100

    Update getUser controller function with proper error handling

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -13,6 +13,13 @@ export const getUser = async (req, res, next) => {
   const { id } = req.params;
   try {
     const user = await User.findById(id);
+
+    // If user does not exist
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw Error;
+    }
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     next(error);
