402a4cf7259774871ae8dac4b78898b5d986eb78
Milos Srejic <srejic.milos94@gmail.com>
2025-03-09 15:02:26 +0100

    Implement controller function for updating existing user

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -18,9 +18,35 @@ export const getUser = async (req, res, next) => {
     if (!user) {
       const error = new Error("User not found");
       error.statusCode = 404;
-      throw Error;
+      throw error;
     }
     res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  const { id } = req.params;
+  const { username, password, email, firstName, lastName } = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      { username, password, email, firstName, lastName },
+      { new: true }
+    );
+
+    // If user does not exist
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "User updated succesffuly", data: user });
   } catch (error) {
     next(error);
   }
